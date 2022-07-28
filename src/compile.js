031b170af27d4a8235dd9cf20463cdf5c0ee0c74
grey <Grey_Jy@yeah.net>
2022-07-28 08:36:39 +0800

    first commit

---
+++ src/compile.js
@@ -0,0 +1,7 @@
+//Compile负责解析模板内容
+class Compile {
+  constructor(el, vm) {
+    this.el = document.querySelector(el)
+    this.vm = vm
+  }
+}
---
+++ src/vue.js
@@ -0,0 +1,12 @@
+//创建Vue实例
+class Vue {
+  constructor(options = {}) {
+    //实例添加属性
+    this.$el = options.el
+    this.$data = options.data
+    if (this.$el) {
+      //Compile负责解析模板内容
+      new Compile(this.$el, this)
+    }
+  }
+}
